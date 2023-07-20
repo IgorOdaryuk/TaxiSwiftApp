64a9dc29586645486d3290d8d8d44c6c7e4c9884
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-20 21:04:55 +0300

    starting new project

---
+++ TaxiApp/App/TaxiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiAppApp.swift
+//  TaxiApp
+//
+//  Created by Igor Odaryuk on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaxiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
