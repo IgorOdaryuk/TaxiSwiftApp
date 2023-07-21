e2dc18fb797f2c9fe938a3129604d22efa978e48
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-21 11:21:32 +0300

    working on UI

---
+++ TaxiApp/Core/Home/Views/MapViewActionButton.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewActionButton.swift
+//  TaxiApp
+//
+//  Created by Igor Odaryuk on 21.07.2023.
+//
+
+import SwiftUI
+
+struct MapViewActionButton: View {
+    var body: some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "line.3.horizontal")
+                .font(.title2)
+                .foregroundColor(.black)
+                .padding()
+                .background(.white)
+                .clipShape(Circle())
+                .shadow(color: .black, radius: 6 )
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct MapViewActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MapViewActionButton()
+    }
+}
