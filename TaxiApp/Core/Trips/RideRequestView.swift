0077f37dffe83554693bf0c6cf4a1640006003c7
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-22 13:07:57 +0300

    update the code

--- TaxiApp/Core/Trips/RideRequestView.swift
+++ TaxiApp/Core/Trips/RideRequestView.swift
@@ -15,6 +15,54 @@ struct RideRequestView: View {
                 .frame(width: 48, height: 6)
             
             //trip info view
+            HStack {
+                //indicator view
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 32)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 8, height: 8)
+                }
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    HStack {
+                        Text("Current location")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text("1:20 PM")
+                            .font(.system(size: 14, weight:
+                                    .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack {
+                        Text("Starbucks Coffee")
+                            .font(.system(size: 16, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        Text("1:45 PM")
+                            .font(.system(size: 14, weight:
+                                    .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            
+            .padding()
+                    
             
             //ride type selection view
             
