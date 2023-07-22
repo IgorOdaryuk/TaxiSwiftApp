19cc294ad0396fdf95fc0defa3c11d8ecc0fe87e
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-22 15:41:11 +0300

    adding a cars to the main UI design

---
+++ TaxiApp/Models/RideType.swift
@@ -0,0 +1,34 @@
+//
+//  RideType.swift
+//  TaxiApp
+//
+//  Created by Igor Odaryuk on 22.07.2023.
+//
+
+import Foundation
+
+
+enum RideType: Int, CaseIterable, Identifiable {
+    case uberX
+    case black
+    case uberXL
+    
+    var id: Int { return rawValue }
+    
+    var description: String {
+        switch self {
+        case .uberX: return "UberX"
+        case .black: return "UberBlack"
+        case .uberXL: return "UberXL"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .uberX: return "uber-x"
+        case .black: return "uber-black"
+        case .uberXL: return "uber-x"
+        }
+    }
+    
+}
