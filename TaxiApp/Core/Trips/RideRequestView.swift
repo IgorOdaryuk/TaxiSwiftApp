c3d8529512c11bd809ae4e27ddb7826b4e46ba37
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-22 12:47:45 +0300

    starting the new day of developing the app, for today it's a rid view

---
+++ TaxiApp/Core/Trips/RideRequestView.swift
@@ -0,0 +1,32 @@
+//
+//  RideRequestView.swift
+//  TaxiApp
+//
+//  Created by Igor Odaryuk on 22.07.2023.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        VStack {
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+            
+            //trip info view
+            
+            //ride type selection view
+            
+            //payment option view
+            
+            //request ride button
+        }
+    }
+}
+
+struct RideRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        RideRequestView()
+    }
+}
