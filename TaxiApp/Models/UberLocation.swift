72ad7131d3b1473c2ffb2a1fdcf7933bcf774d6e
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-22 20:12:46 +0300

    minor fixes

---
+++ TaxiApp/Models/UberLocation.swift
@@ -0,0 +1,13 @@
+//
+//  UberLocation.swift
+//  TaxiApp
+//
+//  Created by Igor Odaryuk on 22.07.2023.
+//
+
+import CoreLocation
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
